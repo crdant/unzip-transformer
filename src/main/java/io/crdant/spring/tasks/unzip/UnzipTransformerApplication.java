b538da3f8e3c13e8f96ff61afe1233fb321ea9e0
Chuck D'Antonio <c_dantonio@acm.org>
2017-01-09 17:26:15 -0500

    Initial commit of generated source files.

---
+++ src/main/java/io/crdant/spring/tasks/unzip/UnzipTransformerApplication.java
@@ -0,0 +1,12 @@
+package io.crdant.spring.tasks.unzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnzipTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnzipTransformerApplication.class, args);
+	}
+}
---
+++ src/test/java/io/crdant/spring/tasks/unzip/UnzipTransformerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.crdant.spring.tasks.unzip;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnzipTransformerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
